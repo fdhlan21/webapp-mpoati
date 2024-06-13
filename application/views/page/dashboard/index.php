5b98d8853af0bac283778b13b5102f7a84bbb4c9
fdhlan21 <fadhlanhimawan12@gmail.com>
2024-06-13 17:06:44 +0700

    13 juni 2024

---
+++ application/views/page/dashboard/index.php
@@ -0,0 +1,29 @@
+<div class="slider">
+    <img src="<?= base_url('assets/img/icon/sliderhome.png'); ?>" alt="Slider Image">
+</div>
+<div class="menu">
+    <div class="menu-item">
+        <a href="<?= base_url('PosyanduBalita'); ?>">
+            <img src="<?= base_url('assets/img/icon/iconbalita.png'); ?>" alt="Posyandu Balita">
+            <p>Posyandu Balita</p>
+        </a>
+    </div>
+    <div class="menu-item">
+        <a href="<?= base_url('PosyanduRemaja'); ?>">
+        <img src="<?= base_url('assets/img/icon/iconremaja.png'); ?>" alt="Posyandu Remaja">
+        <p>Posyandu Remaja</p>
+        </a>
+    </div>
+    <div class="menu-item">
+        <a  href="<?= base_url('Jumantik'); ?>">
+            <img src="<?= base_url('assets/img/icon/iconjumatik.png'); ?>" alt="Jumantik">
+            <p>Jumantik</p>
+        </a>
+    </div>
+    <div class="menu-item">
+        <a href="<?= base_url('Dasawisma'); ?>">
+            <img src="<?= base_url('assets/img/icon/icondasawisma.png'); ?>" alt="Dasawisma">
+            <p>Dasawisma</p>
+        </a>
+    </div>
+</div>
